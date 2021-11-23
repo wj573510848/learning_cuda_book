99d840da5cf0155da06e73e4f68d72fc4d822fde
jian.wang <jian.wang@iluvatar.ai>
2021-11-23 19:42:11 +0800

    update

---
+++ chapter2/hello2.cu
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+__global__ void hello_from_gpu()
+{
+    printf("Hello World from the GPU!\n");
+}
+
+int main(void)
+{
+    hello_from_gpu<<<1,1>>>(); // <<<>>>里面指代 线程块数,每个线程块中的线程数
+    cudaDeviceSynchronize();
+    return 0;
+}
